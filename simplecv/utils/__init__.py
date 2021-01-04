88f6303870227a9ddbb11cab2de93e08ab5565fe
hejian <flystarhe@qq.com>
2021-01-04 14:40:28 +0800

    simplecv/utils/__init__.py - bugs

--- simplecv/utils/__init__.py
+++ simplecv/utils/__init__.py
@@ -17,7 +17,7 @@ def increment_path(path, exist_ok=True, sep=""):
         return make_dir(path)
 
     dirs = glob.glob(f"{path}{sep}*")  # similar paths
-    matches = [re.search(rf"%s{sep}(\d+)" % path.stem, d) for d in dirs]
+    matches = [re.search(rf"%s{sep}(\d+)" % path.name, d) for d in dirs]
     i = [int(m.groups()[0]) for m in matches if m]  # indices
     n = max(i) + 1 if i else 2  # increment number
     return make_dir(f"{path}{sep}{n}")
