389589b18a2738fc1c549e7ea251c0cf35711bd9
hejian <flystarhe@qq.com>
2021-02-02 09:35:15 +0800

    simplecv/mmdet_v2/ext_pipelines/transforms.py - from cvtk

--- simplecv/mmdet_v2/ext_pipelines/transforms.py
+++ simplecv/mmdet_v2/ext_pipelines/transforms.py
@@ -1,3 +1,5 @@
+"""Transforms for mmdet2.x
+"""
 import cv2 as cv
 import numpy as np
 from collections import defaultdict
@@ -15,17 +17,16 @@ class RandomCrop(object):
         uint8,
     """
 
-    def __init__(self, height, width, p=1.0):
-        area = min(width // 3, 112) ** 2
-        self.nonignore_area = area
+    def __init__(self, height, width, **kw):
+        area = (height * width) * 0.5
+        self.nonignore = area
         self.height = height
         self.width = width
-        self.p = p
 
     def _index_selection(self, labels):
         counter = defaultdict(list)
-        for ind, label in enumerate(labels):
-            counter[label].append(ind)
+        for index, label in enumerate(labels):
+            counter[label].append(index)
 
         key = np.random.choice(list(counter.keys()))
         return np.random.choice(counter[key])
@@ -33,16 +34,30 @@ class RandomCrop(object):
     def _get_patch(self, x_min, y_min, x_max, y_max, x_pad, y_pad):
         x0 = np.random.randint(x_min - x_pad, x_max + x_pad - self.width)
         y0 = np.random.randint(y_min - y_pad, y_max + y_pad - self.height)
-        return np.array([x0, y0, x0 + self.width, y0 + self.height], dtype="int64")
+        return x0, y0, x0 + self.width, y0 + self.height
 
-    def _clip_bboxes(self, patch, bboxes):
+    def _crop_and_paste(self, img, patch):
+        img_h, img_w, img_c = img.shape
+        x1, y1, x2, y2 = patch
+
+        p1, x1 = (0, x1) if x1 >= 0 else (-x1, 0)
+        q1, y1 = (0, y1) if y1 >= 0 else (-y1, 0)
+
+        p2, x2 = (self.width, x2) if x2 <= img_w else (img_w - x2, img_w)
+        q2, y2 = (self.height, y2) if y2 <= img_h else (img_h - y2, img_h)
+
+        dst_img = np.zeros((self.height, self.width, img_c), dtype=img.dtype)
+        dst_img[q1: q2, p1: p2] = img[y1: y2, x1: x2]
+        return dst_img
+
+    def _clip_bboxes(self, bboxes, patch):
         """
         Args:
-            patch (ndarray): shape (4,)
-            bboxes (ndarray): shape (k, 4)
+            bboxes (ndarray): shape `(k, 4)`, dtype `np.float32`
+            patch (ndarray): shape `(4,)` or tuple `(x1, y1, x2, y2)`
         """
-        bboxes[:, 2:] = bboxes[:, 2:].clip(max=patch[2:])
-        bboxes[:, :2] = bboxes[:, :2].clip(min=patch[:2])
+        bboxes[:, 0::2] = np.clip(bboxes[:, 0::2], patch[0], patch[2])
+        bboxes[:, 1::2] = np.clip(bboxes[:, 1::2], patch[1], patch[3])
         bboxes -= np.tile(patch[:2], 2)
         return bboxes
 
@@ -55,30 +70,17 @@ class RandomCrop(object):
         dst_h = dst_bboxes[:, 3] - dst_bboxes[:, 1]
         dst_area = dst_w * dst_h
 
-        inner = (dst_w >= 4) * (dst_h >= 4) * (dst_area >= 64)
+        inner = (dst_w >= 4) * (dst_h >= 4) * (dst_area >= 48)
+        x = np.clip(np.sqrt(src_area), 32, None)
+        x = 0.5 + 0.5 * 32 / x
 
-        s1 = (dst_area >= src_area * 0.8)
-        s2 = (dst_area >= self.nonignore_area)
-        s3 = (dst_h >= src_h * 0.9) * (dst_w >= src_h * 2.0)
-        s4 = (dst_w >= src_w * 0.9) * (dst_h >= src_w * 2.0)
+        s1 = (dst_area >= src_area * x)
+        s2 = (dst_area >= self.nonignore)
+        s3 = (dst_w >= src_w) * (dst_h >= src_w * 2.0)
+        s4 = (dst_h >= src_h) * (dst_w >= src_h * 2.0)
+        ss = s1 + s2 + s3 + s4
 
-        dst = s1 + s2 + s3 + s4
-        drop = np.logical_not(dst)
-        return (dst * inner), (drop * inner)
-
-    def _crop_and_paste(self, patch, img):
-        x1, y1, x2, y2 = patch
-        img_h, img_w = img.shape[:2]
-
-        p1, x1 = (-x1, 0) if x1 < 0 else (0, x1)
-        q1, y1 = (-y1, 0) if y1 < 0 else (0, y1)
-
-        p2, x2 = (img_w - x2, img_w) if x2 > img_w else (self.width, x2)
-        q2, y2 = (img_h - y2, img_h) if y2 > img_h else (self.height, y2)
-
-        dst_img = np.zeros((self.height, self.width, 3), dtype=img.dtype)
-        dst_img[q1: q2, p1: p2] = img[y1: y2, x1: x2]
-        return dst_img
+        return ss * inner, np.logical_not(ss) * inner
 
     def __call__(self, results):
         img, bboxes, labels = [results[k] for k in ("img", "gt_bboxes", "gt_labels")]
@@ -87,14 +89,16 @@ class RandomCrop(object):
 
         if bboxes.shape[0] == 0:
             x_min, y_min, x_max, y_max = 0, 0, img_w, img_h
-            x_pad, y_pad = self.width // 2 - 32, self.height // 2 - 32
+
+            x_pad = max(self.width - img_w, 64)
+            y_pad = max(self.height - img_h, 64)
             patch = self._get_patch(x_min, y_min, x_max, y_max, x_pad, y_pad)
 
-            dst_img = self._crop_and_paste(patch, img)
+            dst_img = self._crop_and_paste(img, patch)
 
             cx, cy = self.width // 2, self.height // 2
             x1, y1, x2, y2 = cx - 32, cy - 32, cx + 32, cy + 32
-            dst_img[y1: y2, x1: x2] = dst_img[y1: y2, x1: x2] - 100
+            dst_img[y1: y2, x1: x2] = dst_img[y1: y2, x1: x2] + 128
             dst_bboxes = np.array([[x1, y1, x2, y2]], dtype=np.float32)  # man-made object
             dst_labels = np.array([0], dtype=np.int64)  # set the man-made object category in 1st
 
@@ -106,22 +110,18 @@ class RandomCrop(object):
             results["gt_labels"] = dst_labels
             return results
 
-        if img_h <= self.height and img_w <= self.width:
-            return results
-
-        bboxes = bboxes.astype("int64")
         index = self._index_selection(labels)
-        x_min, y_min, x_max, y_max = bboxes[index]
-        x_pad, y_pad = self.width // 3 * 2, self.height // 3 * 2
+        x_min, y_min, x_max, y_max = map(int, bboxes[index])
+
+        x_pad = max(self.width - (x_max - x_min), 64)
+        y_pad = max(self.height - (y_max - y_min), 64)
         patch = self._get_patch(x_min, y_min, x_max, y_max, x_pad, y_pad)
 
-        dst_img = self._crop_and_paste(patch, img)
-
-        dst_bboxes = self._clip_bboxes(patch, bboxes.copy())
+        dst_img = self._crop_and_paste(img, patch)
+        dst_bboxes = self._clip_bboxes(bboxes.copy(), patch)
         dst_mask, drop_mask = self._check_bboxes(bboxes, dst_bboxes)
         for x1, y1, x2, y2 in dst_bboxes[drop_mask]:
             dst_img[y1: y2, x1: x2] = 0
-        dst_bboxes = dst_bboxes.astype("float32")
         dst_bboxes = dst_bboxes[dst_mask]
         dst_labels = labels[dst_mask]
 
@@ -136,7 +136,7 @@ class RandomCrop(object):
 
 class Resize2(object):
 
-    def __init__(self, test_mode=False, ratio_range=(0.8, 1.2), **kwargs):
+    def __init__(self, test_mode=False, ratio_range=(0.8, 1.2), **kw):
         self.test_mode = test_mode
         self.ratio_range = ratio_range
 
